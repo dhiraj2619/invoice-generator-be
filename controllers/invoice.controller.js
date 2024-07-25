8e1382c567575df2ea3c21f1d2c846af4d40f34b
Dhiraj <dhirajmymodak22@gmail.com>
2024-07-25 10:58:07 +0530

    invoice num date issue fixed

--- controllers/invoice.controller.js
+++ controllers/invoice.controller.js
@@ -2,21 +2,21 @@ const upload = require("../config/multer.config");
 const Invoice = require("../models/invoice.model")
 
 
-async function generateInvoiceNumber(){
+async function generateInvoiceNumber() {
     const prefixInvoice = "IN-";
     const randomNumber = Math.floor(Math.random() * 1000);
     const invoiceNo = `${prefixInvoice}${randomNumber}`;
 
-    const existingInvoiceNumber = await Invoice.findOne({invoiceNo});
+    const existingInvoiceNumber = await Invoice.findOne({ invoiceNo });
 
-    if(existingInvoiceNumber){
-          return generateInvoiceNumber();
+    if (existingInvoiceNumber) {
+        return generateInvoiceNumber();
     }
     return invoiceNo;
 }
 const InvoiceController = {
     addInvoice: async (req, res) => {
-        upload.single('companylogo')(req,res,async(err)=>{
+        upload.single('companylogo')(req, res, async (err) => {
             if (err) {
                 return res.status(400).json({ message: err.message });
             }
@@ -26,15 +26,28 @@ const InvoiceController = {
 
             const companylogo = req.file.path;
             const invoiceNo = await generateInvoiceNumber();
+            const today = new Date();
+            const day = String(today.getDate()).padStart(2, '0');
+            const month = String(today.getMonth() + 1).padStart(2, '0');
+            const year = today.getFullYear();
+            const invoiceDate = `${day}-${month}-${year}`;
+
             try {
-                const newInvoice = new Invoice({...req.body,companylogo,invoiceNo});
+                const newInvoice = new Invoice({
+                    ...req.body, companylogo,
+                    invoiceDetails: {
+                        ...req.body.invoiceDetails,
+                        invoiceNo,
+                        invoiceDate
+                    }
+                });
                 const savedInvoice = await newInvoice.save();
                 return res.status(201).json({ message: "invoice saved successfully", savedInvoice })
             } catch (error) {
                 res.status(400).json({ message: error.message });
             }
         })
-      
+
     },
     getAllinvoices: async (req, res) => {
         try {
