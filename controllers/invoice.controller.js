1164a24c1d6fc35e9829c82f6e10349d59874fc9
Dhiraj <dhirajmymodak22@gmail.com>
2024-07-25 21:27:27 +0530

    invoice controller changed

--- controllers/invoice.controller.js
+++ controllers/invoice.controller.js
@@ -1,6 +1,5 @@
 const upload = require("../config/multer.config");
-const Invoice = require("../models/invoice.model")
-
+const Invoice = require("../models/invoice.model");
 
 async function generateInvoiceNumber() {
     const prefixInvoice = "IN-";
@@ -14,6 +13,7 @@ async function generateInvoiceNumber() {
     }
     return invoiceNo;
 }
+
 const InvoiceController = {
     addInvoice: async (req, res) => {
         upload.single('companylogo')(req, res, async (err) => {
@@ -56,9 +56,9 @@ const InvoiceController = {
                 res.status(400).json({ message: error.message });
             }
         });
-
     },
-    getAllinvoices: async (req, res) => {
+
+    getAllInvoices: async (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         try {
             const invoices = await Invoice.find().populate('seller');
@@ -67,19 +67,21 @@ const InvoiceController = {
             res.status(500).json({ message: error.message });
         }
     },
+
     getInvoiceById: async (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         try {
             const invoice = await Invoice.findById(req.params.id).populate('seller');
 
             if (!invoice) {
-                return res.status(400).json({ message: "invoice not found" })
+                return res.status(404).json({ message: "Invoice not found" });
             }
             res.status(200).json(invoice);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     },
+
     updateInvoice: async (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         try {
@@ -87,11 +89,12 @@ const InvoiceController = {
             if (!updatedInvoice) {
                 return res.status(404).json({ message: 'Invoice not found' });
             }
-            return res.status(200).json({ message: "invoice updated successfully", updatedInvoice });
+            return res.status(200).json({ message: "Invoice updated successfully", updatedInvoice });
         } catch (error) {
             res.status(400).json({ message: error.message });
         }
     },
+
     deleteInvoice: async (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*'); 
         try {
@@ -106,4 +109,4 @@ const InvoiceController = {
     }
 }
 
-module.exports = InvoiceController
+module.exports = InvoiceController;
