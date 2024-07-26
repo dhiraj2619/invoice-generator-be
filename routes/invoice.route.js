b9d0b896710a96e75d6e0eff9132e427b5dec47a
Dhiraj <dhirajmymodak22@gmail.com>
2024-07-26 09:33:43 +0530

    adjusting route

--- routes/invoice.route.js
+++ routes/invoice.route.js
@@ -3,8 +3,8 @@ const InvoiceController = require('../controllers/invoice.controller');
 const invoiceRouter = express.Router();
 
 invoiceRouter.post('/add',InvoiceController.addInvoice);
-invoiceRouter.get('/:id',InvoiceController.getInvoiceById);
 invoiceRouter.get('/allinvoice',InvoiceController.getAllinvoices);
+invoiceRouter.get('/:id',InvoiceController.getInvoiceById);
 invoiceRouter.put('/:id',InvoiceController.updateInvoice);
 invoiceRouter.delete('/:id',InvoiceController.deleteInvoice);
 
